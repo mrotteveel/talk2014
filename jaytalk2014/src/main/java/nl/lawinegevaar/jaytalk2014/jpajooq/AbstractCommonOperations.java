23e16f365bf388e36335c27d9a27b967b123936d
Mark Rotteveel <mark+github@lawinegevaar.nl>
2014-10-22 11:26:19 +0200

    Only create EntityManagerFactory when using JPA; don't set character set as employee db is charset NONE.

--- jaytalk2014/src/main/java/nl/lawinegevaar/jaytalk2014/jpajooq/AbstractCommonOperations.java
+++ jaytalk2014/src/main/java/nl/lawinegevaar/jaytalk2014/jpajooq/AbstractCommonOperations.java
@@ -30,21 +30,9 @@ public abstract class AbstractCommonOperations {
     @Rule
     public TestName name = new TestName();
 
-    private static final String JDBC_URL = "jdbc:firebirdsql://localhost/D:/data/db/fb3/employee_talk.fdb?charSet=UTF-8";
+    private static final String JDBC_URL = "jdbc:firebirdsql://localhost/D:/data/db/fb3/employee_talk.fdb";
     private static final String USER = "SYSDBA";
     private static final String PASSWORD = "masterkey";
-
-    private static EntityManagerFactory emf;
-
-    @BeforeClass
-    public static void init() {
-        emf = Persistence.createEntityManagerFactory("employee");
-    }
-
-    @AfterClass
-    public static void cleanup() {
-        emf.close();
-    }
 
     @Before
     public void printTestName() {
@@ -64,11 +52,16 @@ public abstract class AbstractCommonOperations {
     }
 
     protected void withJpa(Consumer<EntityManager> actionWithJpa) {
-        EntityManager em = createEntityManager();
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("employee");
         try {
-            actionWithJpa.accept(em);
+            EntityManager em = emf.createEntityManager();
+            try {
+                actionWithJpa.accept(em);
+            } finally {
+                em.close();
+            }
         } finally {
-            em.close();
+            emf.close();
         }
     }
 
@@ -81,10 +74,6 @@ public abstract class AbstractCommonOperations {
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
-    }
-
-    protected EntityManager createEntityManager() {
-        return emf.createEntityManager();
     }
 
     protected Connection getConnection() throws SQLException {
